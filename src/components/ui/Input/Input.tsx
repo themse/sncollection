d26e8eff22d92beb32b95b74fe356cebd6c01643
Den Polusmyak <den.polusmyak@gmail.com>
2023-09-05 02:08:08 +0300

    implement input

---
+++ src/components/ui/Input/Input.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { forwardRef, InputHTMLAttributes, useState } from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@helpers/cn';
+
+const inputVariants = cva(
+	[
+		'outline-blue flex h-12 min-w-[295px] w-fit rounded-lg border border-gray-200 px-4 py-3 outline-2 placeholder:text-gray-200',
+		'hover:border-gray-400',
+		'active:border-2 active:border-black active:outline-none',
+		'disabled:cursor-not-allowed disabled:opacity-50',
+		'focus-within:border-2 focus-within:border-black',
+	],
+	{
+		variants: {
+			fullWidth: {
+				true: 'w-full',
+			},
+			hasError: {
+				true: 'border-red',
+			},
+		},
+	},
+);
+
+interface InputProps
+	extends InputHTMLAttributes<HTMLInputElement>,
+		VariantProps<typeof inputVariants> {}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+	({ className, fullWidth, hasError, type = 'text', ...props }, ref) => {
+		const [isActive, setIsActive] = useState(false);
+
+		return (
+			<input
+				onClick={() => setIsActive(true)}
+				onBlur={() => setIsActive(false)}
+				type={type}
+				className={cn(
+					[inputVariants({ fullWidth, hasError }), isActive && 'focus-within:outline-none'],
+					className,
+				)}
+				ref={ref}
+				{...props}
+			/>
+		);
+	},
+);
+
+Input.displayName = 'Input';
