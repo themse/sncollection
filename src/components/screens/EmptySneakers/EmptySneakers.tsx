128f6e48fb1171ab5c6f513539a76c30d122e8cf
Den Polusmyak <den.polusmyak@gmail.com>
2023-09-09 19:56:16 +0300

    improve image loading

--- src/components/screens/EmptySneakers/EmptySneakers.tsx
+++ src/components/screens/EmptySneakers/EmptySneakers.tsx
@@ -7,7 +7,7 @@ export const EmptySneakers = () => {
 	return (
 		<div className="flex w-full flex-col justify-center gap-y-10 py-14">
 			<div className="mx-auto">
-				<Image src={emptyImg} alt="Empty Page" className="object-contain" />
+				<Image src={emptyImg} alt="Empty Page" className="object-contain" priority />
 			</div>
 			<Text className="text-center">
 				Seem&apos;s like you still didn&apos;t add
--- src/components/screens/NotFoundSneakers/NotFoundSneakers.tsx
+++ src/components/screens/NotFoundSneakers/NotFoundSneakers.tsx
@@ -7,7 +7,12 @@ export const NotFoundSneakers = () => {
 	return (
 		<div className="flex w-full flex-col justify-center gap-y-10 py-14">
 			<div className="mx-auto">
-				<Image src={notFoundImg} alt="Not Found Sneakers Page" className="object-contain" />
+				<Image
+					src={notFoundImg}
+					alt="Not Found Sneakers Page"
+					className="object-contain"
+					priority
+				/>
 			</div>
 			<Text className="text-center">
 				Search better. <br /> There is nothing like this in your collection.
